dd6adc7268f3e969eae87f8610565aa5f176d8e9
Дмитрий <ecs.kybo@yandex.ru>
2026-02-04 15:02:42 +0300

    Пофиксил тест buzzTest.spec.ts

--- src/pages/buzzPage/buzzPage.ts
+++ src/pages/buzzPage/buzzPage.ts
@@ -8,12 +8,32 @@ export class BuzzPage{
     readonly textPage: Locator
     readonly navigationBuzzName: Locator
 
+    readonly opensymbolMenu: Locator
+    readonly deletePost: Locator
+    readonly h1TextMenu: Locator
+    readonly h2Textmenu: Locator
+    readonly buttonNo: Locator
+    readonly buttonYes: Locator
+    readonly buttonExit: Locator
+    readonly editPost: Locator
+
+
     constructor(page: Page) {
         this.page = page
         this.textInput = this.page.getByRole('textbox', { name: "What's on your mind?"})
         this.postButton = this.page.getByRole('button', { name: 'Post', exact: true })
         this.textPage = this.page.getByText('Buzz Newsfeed')
         this.navigationBuzzName = this.page.getByRole('link', {name: 'Buzz'})
+
+        this.opensymbolMenu = this.page.getByRole('button').filter({ hasText: /^$/ }).nth(2)
+        this.deletePost = this.page.getByRole('listitem').filter({ hasText: /^Delete Post$/ })
+        this.h1TextMenu = this.page.getByText('Are you Sure?')
+        this.h2Textmenu = this.page.getByText('The selected item will be')
+        this.buttonNo = this.page.getByRole('button', { name: 'No, Cancel' })
+        this.buttonYes = this.page.getByRole('button', { name: ' Yes, Delete' })
+        this.buttonExit = this.page.getByRole('button', { name: '×' })
+        this.editPost = this.page.getByText('Edit Post')
+
     }
 
     async visit(){
@@ -26,15 +46,32 @@ export class BuzzPage{
 
     async postMessage(messageText: string){
         await test.step('Написание текста в поле', async () => {
-            await this.textInput.click()
-            await this.textInput.fill(messageText)
+            await this.textInput.click({timeout: 5000})
+            await this.textInput.fill(messageText, {timeout: 5000})
             await expect(this.textInput).toHaveValue(messageText)
             await this.postButton.click()
         })
     }
     async expectPostMessage(messageText: string){
-        await test.step('Проверка оставленного комментария', async(done) => {
-            await expect(this.page.getByText(messageText)).toBeVisible()
+        await test.step('Проверка оставленного комментария', async() => {
+            const latestPost = this.page.getByText(messageText).first()
+            await expect(latestPost).toBeVisible({timeout: 5000})
+        })
+    }
+
+    async exdeletePost(messageText: string){
+        await test.step('Удаление комментария', async() => {
+            await this.opensymbolMenu.click()
+            await expect(this.deletePost).toBeVisible()
+            await this.deletePost.click()
+            await expect(this.h1TextMenu).toBeVisible()
+            await expect(this.h2Textmenu).toBeVisible()
+            await expect(this.buttonNo).toBeVisible()
+            await expect(this.buttonYes).toBeVisible()
+            //await expect(this.buttonExit).toBeVisible()
+            await this.buttonYes.click()
+            const latestPost = this.page.getByText(messageText).last()
+            await expect(latestPost).not.toBeVisible({ timeout: 5000 })
         })
     }
 }
